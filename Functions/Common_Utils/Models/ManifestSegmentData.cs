2342e67ac653c3291096153ed2544c2bc793166a
xpouyat@microsoft.com <xpouyat@microsoft.com>
2021-07-20 14:43:19 +0200

    start

---
+++ Functions/Common_Utils/Models/ManifestSegmentData.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Common_Utils
+{
+    public class ManifestSegmentData
+    {
+        public ulong timestamp;
+        public bool calculated; // it means the timestamp has been calculated from previous
+        public bool timestamp_mismatch; // if there is a mismatch
+    }
+}
---
+++ Functions/Common_Utils/Models/ManifestTimingData.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+
+namespace Common_Utils
+{
+    public class ManifestTimingData
+    {
+        public TimeSpan AssetDuration { get; set; }
+        public ulong TimestampOffset { get; set; }
+        public long? TimeScale { get; set; }
+        public bool IsLive { get; set; }
+        public bool Error { get; set; }
+        public List<ulong> TimestampList { get; set; }
+        public ulong TimestampEndLastChunk { get; set; }
+        public bool DiscontinuityDetected { get; set; }
+    }
+
+}
